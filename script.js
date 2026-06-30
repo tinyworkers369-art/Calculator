94610d61f2cc1a8e22f53da982a6869cbfb45a42
tinyworkers369-art <tinyworkers369@gmail.com>
2026-06-30 21:34:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.innerText;
+
+        if (value === "AC") {
+            expression = "";
+            display.value = "";
+        }
+
+        else if (value === "DEL") {
+            expression = expression.slice(0, -1);
+            display.value = expression;
+        }
+
+        else if (value === "=") {
+            try {
+                let exp = expression
+                    .replace(/×/g, "*")
+                    .replace(/÷/g, "/")
+                    .replace(/−/g, "-");
+
+                display.value = eval(exp);
+                expression = display.value;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+
+        else {
+            expression += value;
+            display.value = expression;
+        }
+    });
+});
